b41c0069c3931d1aa0b3580f021b6352900ed635
Om Arora <iamomthegreat@gmail.com>
2020-11-05 11:28:05 +0530

    Added a nice looking navbar with icons which change colour on hover. Also added the Blanka ttf file. Hope it appears ok on github site.

---
+++ main.js
@@ -0,0 +1,31 @@
+/*
+function changehome_hover() {
+  element = document.getElementById('home');
+  element.setAttribute('color', '#000');
+}
+
+function changehome_unhover() {
+  element = document.getElementById('home');
+  element.setAttribute('color', '#fff');
+}
+*/
+
+function changevideogame_hover() {
+  element = document.getElementById('videogame');
+  element.setAttribute('src', 'videogame-pink.svg');
+}
+
+function changevideogame_unhover() {
+  element = document.getElementById('videogame');
+  element.setAttribute('src', 'videogame.svg');
+}
+
+function changeteam_hover() {
+  element = document.getElementById('team');
+  element.setAttribute('src', 'team-pink.svg');
+}
+
+function changeteam_unhover() {
+  element = document.getElementById('team');
+  element.setAttribute('src', 'team.svg');
+}
